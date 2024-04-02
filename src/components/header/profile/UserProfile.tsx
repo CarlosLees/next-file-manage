0058e68e4a8aa29d32fc13edb9d7ca95487f2fa5
Carlos <61411632+CarlosLees@users.noreply.github.com>
2024-04-02 16:41:42 +0800

    fix header navbar css

--- src/components/header/profile/UserProfile.tsx
+++ src/components/header/profile/UserProfile.tsx
@@ -5,11 +5,13 @@ import styles from './userProfile.module.css';
 const UserProfile = () => {
     return (
         <div className={styles.container}>
-            <Image src="/header-message.svg" alt="message" width={20} height={20} />
+            <div className={styles.message}>
+                <Image src="/header-message.svg" alt="message" fill className={styles.image} />
+            </div>
             <div className={styles.username}>Username</div>
 
             <div className={styles.dropdown}>
-                <Image src="/header-avatar.png" alt="头像" width={25} height={25} />
+                <Image src="/header-avatar.png" alt="头像" fill />
             </div>
         </div>
     );
